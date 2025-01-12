8765c6b9ae37eca2660218534a6bf9ffe827951a
junsuPark <krpjs0508@gmail.com>
2025-01-12 17:31:05 +0900

    click count

--- app/src/main/java/app/junsu/popcat/MainActivity.kt
+++ app/src/main/java/app/junsu/popcat/MainActivity.kt
@@ -7,15 +7,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -24,7 +28,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.junsu.popcat.ui.theme.PopCatTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,9 +52,12 @@ fun Popcat(
     modifier: Modifier = Modifier,
 ) {
     var pressed by remember { mutableStateOf(false) }
+    var clickCount by remember { mutableIntStateOf(0) }
+
     val context = LocalContext.current
     val soundPool = remember { SoundPool.Builder().setMaxStreams(10).build() }
     var popSoundId: Int? = null
+
 
     LaunchedEffect(Unit) {
         popSoundId = soundPool.load(context, R.raw.pop_sound, 1)
@@ -60,35 +69,48 @@ fun Popcat(
         Box(
             modifier = Modifier
                 .padding(innerPadding)
-                .pointerInput(Unit) {
-                    detectTapGestures(
-                        onPress = {
-                            soundPool.play(popSoundId!!, 1f, 1f, 0, 0, 1f)
-                            pressed = true
-                            try {
-                                awaitRelease()
-                            } finally {
-                                pressed = false
-                            }
-                        },
-                    )
-                }
                 .fillMaxSize(),
             contentAlignment = Alignment.Center,
         ) {
-            Image(
-                painter = painterResource(
-                    if (pressed) {
-                        R.drawable.popcat_open
-                    } else {
-                        R.drawable.popcat_closed
-                    },
+            Column(
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 16.dp,
                 ),
-                contentDescription = "Popcat Icon",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(400.dp),
-            )
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painterResource(
+                        if (pressed) {
+                            R.drawable.popcat_open
+                        } else {
+                            R.drawable.popcat_closed
+                        },
+                    ),
+                    contentDescription = "Popcat Icon",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onPress = {
+                                    soundPool.play(popSoundId!!, 1f, 1f, 0, 0, 1f)
+                                    clickCount++
+                                    pressed = true
+                                    try {
+                                        awaitRelease()
+                                    } finally {
+                                        pressed = false
+                                    }
+                                },
+                            )
+                        }
+                        .height(400.dp),
+                )
+                Text(
+                    "Current count: $clickCount",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
         }
     }
 }
