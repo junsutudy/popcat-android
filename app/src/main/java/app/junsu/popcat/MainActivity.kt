a4f24db689a0a5979f76c30d54ab641ee9565daf
junsuPark <krpjs0508@gmail.com>
2025-01-12 17:00:59 +0900

    click

--- app/src/main/java/app/junsu/popcat/MainActivity.kt
+++ app/src/main/java/app/junsu/popcat/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,8 +13,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import app.junsu.popcat.ui.theme.PopCatTheme
@@ -36,17 +42,37 @@ class MainActivity : ComponentActivity() {
 fun Popcat(
     modifier: Modifier = Modifier,
 ) {
+    var pressed by remember { mutableStateOf(false) }
+
     Scaffold(
         modifier = modifier.fillMaxSize(),
     ) { innerPadding ->
         Box(
             modifier = Modifier
                 .padding(innerPadding)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onPress = {
+                            pressed = true
+                            try {
+                                awaitRelease()
+                            } finally {
+                                pressed = false
+                            }
+                        },
+                    )
+                }
                 .fillMaxSize(),
             contentAlignment = Alignment.Center,
         ) {
             Image(
-                painter = painterResource(R.drawable.popcat_closed),
+                painter = painterResource(
+                    if (pressed) {
+                        R.drawable.popcat_open
+                    } else {
+                        R.drawable.popcat_closed
+                    },
+                ),
                 contentDescription = "Popcat Icon",
                 modifier = Modifier
                     .fillMaxWidth()
